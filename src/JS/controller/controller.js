0ac26fc43f0c0440cd5b78f2e412f22ab9fed488
LeslieInostroza <leslie.ino@gmail.com>
2018-08-17 13:41:48 -0300

    se divide en vista controlador y modelo

---
+++ src/JS/controller/controller.js
@@ -0,0 +1,10 @@
+btnSearchModal.addEventListener('click', () => {
+  cleanDescription();
+  const ubication = `https://places.cit.api.here.com/places/v1/discover/search?app_id=fLzneZsZJmJNOTDlBulj&app_code=dWYEJbbiTjECkQ-bI243qQ&at=${HEREHQcoordinates.lat},${HEREHQcoordinates.lng}&q=${inputGroupSelect.value}&pretty`
+  fetch(ubication)
+  .then(response => response.json())
+  .then(explorer => {
+    console.log(explorer);
+    renderInfo(explorer);
+  });
+});
--- src/JS/views/views.js
+++ src/JS/views/views.js
@@ -1,9 +1,10 @@
-btnSearchModal.addEventListener('click', () => {
-  const ubication = `https://places.cit.api.here.com/places/v1/discover/search?app_id=fLzneZsZJmJNOTDlBulj&app_code=dWYEJbbiTjECkQ-bI243qQ&at=${HEREHQcoordinates.lat},${HEREHQcoordinates.lng}&q=${inputGroupSelect.value}&pretty`
-  fetch(ubication)
-  .then(response => response.json())
-  .then(explorer => {
-    console.log(explorer);
-    renderInfo(explorer);
-  });
-});
+const renderInfo = (explorer) => {
+  containerRenderMap.innerHTML += `<p>${JSON.stringify(explorer.results.items[0].title)}
+  direccion: ${JSON.stringify(explorer.results.items[0].vicinity)}</p> <p>${JSON.stringify(explorer.results.items[1].title)}
+  direccion: ${JSON.stringify(explorer.results.items[1].vicinity)}</p>
+  <p>${JSON.stringify(explorer.results.items[2].title)}
+  direccion: ${JSON.stringify(explorer.results.items[2].vicinity)}</p>`;
+}
+const cleanDescription = () => {
+  containerRenderMap.innerHTML = '';
+}
