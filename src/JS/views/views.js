794c265f01793841b71353ead33512151497a77b
LeslieInostroza <leslie.ino@gmail.com>
2018-08-17 19:59:08 -0300

    modal y diseño

--- src/JS/views/views.js
+++ src/JS/views/views.js
@@ -1,10 +1,31 @@
 const renderInfo = (explorer) => {
-  containerRenderMap.innerHTML += `<p>${JSON.stringify(explorer.results.items[0].title)}
-  direccion: ${JSON.stringify(explorer.results.items[0].vicinity)}</p> <p>${JSON.stringify(explorer.results.items[1].title)}
-  direccion: ${JSON.stringify(explorer.results.items[1].vicinity)}</p>
-  <p>${JSON.stringify(explorer.results.items[2].title)}
-  direccion: ${JSON.stringify(explorer.results.items[2].vicinity)}</p>`;
-}
+  explorer.results.items.forEach((item)=>{
+    let mapInfo = {
+      name: item.title,
+      direccion: item.vicinity,
+      distancia: item.distance
+    }
+    /*containerRenderMap.innerHTML += `
+    <p>${mapInfo.name} esta a: ${mapInfo.distancia} metros, de su posicion actual.</p>
+    <p>${mapInfo.direccion}</p>`;*/ //funciona pero muestra los 20 mas cercano
+  });
+  containerRenderMap.innerHTML += `
+  <div class="divModel">
+    <p class="pTitle">${JSON.stringify(explorer.results.items[0].title)}</p>
+    <p>Calificacion: ${JSON.stringify(explorer.results.items[0].averageRating)}</p>
+    <p>Direccion: ${JSON.stringify(explorer.results.items[0].vicinity)}</p>
+  </div>
+  <div class="divModel">
+    <p class="pTitle">${JSON.stringify(explorer.results.items[1].title)}</p>
+    <p>Calificacion: ${JSON.stringify(explorer.results.items[1].averageRating)}</p>
+    <p>Direccion: ${JSON.stringify(explorer.results.items[1].vicinity)}</p>
+  </div>
+  <div class="divModel">
+    <p class="pTitle">${JSON.stringify(explorer.results.items[2].title)}</p>
+    <p>Calificacion: ${JSON.stringify(explorer.results.items[2].averageRating)}</p>
+    <p>Direccion: ${JSON.stringify(explorer.results.items[2].vicinity)}</p>
+  </div>`;
+};
 const cleanDescription = () => {
   containerRenderMap.innerHTML = '';
-}
+};
