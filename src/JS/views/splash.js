343ecbc63a46ff1c332496b7043d8cffb8d141d3
LeslieInostroza <leslie.ino@gmail.com>
2018-08-17 21:37:10 -0300

    splash

---
+++ src/JS/views/splash.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  headerTitle.style.display ="block";
+  headerTitle.style.animation = 'fadeout 1s ease';
+  inputModal.style.animation = 'fadein 1s ease';
+  sectionMap.style.animation = 'fadein 1s ease';
+  footer.style.animation = 'fadein 1s ease';
+  inputModal.style.display ="block";
+  footer.style.display ="block";
+}, 2500);
